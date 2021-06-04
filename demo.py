9735e4b56b89e92905d4ba372e09ead0ce9f4a31
Purnima Suryaprakash <purnima1999.surya@gmail.com>
2021-06-04 20:37:39 +0530

    Fully functional docker sample added

---
+++ demo.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+app = Flask(__name__,template_folder='templates')
+  
+@app.route('/')
+def hello():
+    #return "welcome to the flask tutorials"
+    return render_template('home.html')
+  
+  
+if __name__ == "__main__":
+    app.run(host ='0.0.0.0', port = 5001, debug = True) 
